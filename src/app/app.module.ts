c8eb803e888225de93c5739ac2bd06a34f09270f
Lean <leian1306@gmail.com>
2023-01-29 15:46:13 +0100

    src app search: move the component to its own module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import { SearchModule } from './search/search.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    SearchComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SearchModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/search/search.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+
+import { SearchComponent } from './search.component';
+
+@NgModule({
+  declarations: [SearchComponent],
+})
+export class SearchModule {}
