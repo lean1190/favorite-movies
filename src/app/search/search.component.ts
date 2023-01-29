34428aef999cd3f3a63cfdb32720d406338b276c
Lean <leian1306@gmail.com>
2023-01-29 16:04:03 +0100

    src app search component: add a form group for dealing with the input changes in the search

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map, Observable, switchMap } from 'rxjs';
+
+import { MoviesGroup } from './interfaces/movie';
+import { SearchService } from './providers/search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.less']
+  styleUrls: ['./search.component.less'],
+  providers: [SearchService]
 })
 export class SearchComponent {
 
-  constructor() {
+  public searchForm;
+  public movies: Observable<MoviesGroup[]>;
 
+  constructor(formBuilder: FormBuilder, private searchService: SearchService) {
+    this.searchForm = formBuilder.group({
+      title: new FormControl('', Validators.required)
+    });
+
+    this.movies = this.searchForm.controls.title.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      // Search to the api
+      switchMap((title) => this.searchService.searchMovies(title as string))
+    );
   }
 }
